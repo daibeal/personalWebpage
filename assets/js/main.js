0717e96a8c3073d1825811a37446547ab18dcd1b
Dairon Andrés <dairon.aldaz@gmail.com>
2021-06-13 15:48:41 +0200

    D

--- assets/js/main.js
+++ assets/js/main.js
@@ -337,3 +337,11 @@
   }
 
   next()
+  function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: { lat: -33, lng: 151 },
+      disableDefaultUI: true,
+    });
+  }
+  
