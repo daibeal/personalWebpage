81403145801b224ecef524cd145e2d3c872d7688
Dairon Andrés <dairon.aldaz@gmail.com>
2021-06-16 20:49:54 +0200

    Chnagea

--- assets/js/main.js
+++ assets/js/main.js
@@ -348,14 +348,3 @@ const next = () => {
 }
 
 next()
-
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: {
-      lat: -33,
-      lng: 151
-    },
-    disableDefaultUI: true,
-  });
-}
